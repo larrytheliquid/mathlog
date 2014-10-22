00fc6b2139e519d195397008a7f255fe5276ca1d
Larry Diehl <larrytheliquid@gmail.com>
2014-10-22 09:41:02 -0700

    Finish s4

--- hw2/Hw2.hs
+++ hw2/Hw2.hs
@@ -11,11 +11,34 @@ import Data.Typeable
 
 data Day = Mon | Tue | Wed | Thu | Fri | Sat | Sun
   deriving (Show,Read,Enum,Eq,Ord,Typeable)
+instance Finite Day where
+
+dimR r = Dim (length r) Int [LInt x | x <- r]
 
 d1 = dimS ["Odd", "Even"]
 d2 = dim 10
-d3 = Dim (length r) Int [LInt x | x <- r]
-  where r = [5..10]
+d3 = dimR [5..10]
 d4 = dimE "Day" Mon
+
+----------------------------------------------------------------------
+
+s1 = partial [d1,d3] $ \ [x,y] -> f (unString (indexToLit d1 x)) (unInt (indexToLit d3 y))
+  where
+  f "Even" n = if even n then Just True else Nothing
+  f "Odd" n = if odd n then Just True else Nothing
+
+s2 = partial [d2,d2] $ \ [x,y] -> Just $ unInt (indexToLit d2 x) + unInt (indexToLit d2 y)
+
+s3 = partial [dx,dy] $ \ [x,y] -> unInt (indexToLit dx x) `f` unInt (indexToLit dy y)
+  where
+  f m n = if succ m == n then Just True else Nothing
+  dx = dimR [0..4]
+  dy = dimR [1..5]
+
+s4 = fromFiniteList True [d4,d4] [ (x,y) | x <- enumFrom Mon , y <- enumFrom Mon , x < y ]
+
+----------------------------------------------------------------------
+
+
 
 ----------------------------------------------------------------------
