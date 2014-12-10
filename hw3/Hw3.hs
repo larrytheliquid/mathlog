3124fa2d96b17f39e3563cbc91d79f72bc4866e9
Larry Diehl <larrytheliquid@gmail.com>
2014-12-09 16:36:00 -0800

    Start change

--- hw3/Hw3.hs
+++ hw3/Hw3.hs
@@ -4,63 +4,49 @@ import SimpleProp
 
 ----------------------------------------------------------------------
 
+type SProp a = (Bool, Prop a)
+
+----------------------------------------------------------------------
+
 data Discrim a = Alpha a a | Beta a a | Lit a
 
-discrim :: Prop a -> Discrim (Prop a)
-discrim TruthP = Lit TruthP
-discrim AbsurdP = Lit AbsurdP
-discrim (LetterP s) = Lit (LetterP s)
-discrim (AndP x y) = Alpha x y
-discrim (OrP x y) = Beta x y
-discrim (ImpliesP x y) = Beta (NotP x) y
-discrim (NotP (OrP x y)) = Alpha (NotP x)  (NotP y)
-discrim (NotP (ImpliesP x y)) = Alpha x (NotP y)
-discrim (NotP (AndP x y)) = Beta (NotP x) (NotP y)
-discrim (NotP (NotP x)) = discrim x
-discrim (NotP TruthP) = Lit AbsurdP
-discrim (NotP AbsurdP) = Lit TruthP
-discrim (NotP (LetterP s)) = Lit (NotP (LetterP s))
+discrim :: SProp a -> Discrim (Prop a)
+
+discrim (True , TruthP) = Lit TruthP
+discrim (True , AbsurdP) = Lit AbsurdP
+discrim (True , LetterP s) = Lit (LetterP s)
+discrim (True , AndP x y) = Alpha x y
+discrim (True , OrP x y) = Beta x y
+discrim (True , ImpliesP x y) = Beta (NotP x) y
+discrim (True , NotP x) = undefined
+
+discrim (False , OrP x y) = Alpha (NotP x)  (NotP y)
+discrim (False , ImpliesP x y) = Alpha x (NotP y)
+discrim (False , AndP x y) = Beta (NotP x) (NotP y)
+discrim (False , NotP x) = undefined -- discrim x
+discrim (False , TruthP) = Lit AbsurdP
+discrim (False , AbsurdP) = Lit TruthP
+discrim (False , LetterP s) = Lit (NotP (LetterP s))
 
 ----------------------------------------------------------------------
 
-process :: [Prop a] -> [[Prop a]]
-process [] = [[]]
-process (p : ps) =
-  case (discrim p) of
-    Lit x -> map (x:) (process ps)
-    Alpha x y -> process (x : y : ps)
-    Beta x y -> process (x : ps) ++ process (y : ps)
+-- process :: [Prop a] -> [[Prop a]]
+-- process [] = [[]]
+-- process (p : ps) =
+--   case (discrim p) of
+--     Lit x -> map (x:) (process ps)
+--     Alpha x y -> process (x : y : ps)
+--     Beta x y -> process (x : ps) ++ process (y : ps)
 
-tabulate :: Prop a -> [[Prop a]]
-tabulate p = process [NotP p]
-
-----------------------------------------------------------------------
-
-type ABL a = ( [(Prop a , Prop a)] , [(Prop a , Prop a)] , [Prop a] )
-
-insertABL :: Prop a -> ABL a -> ABL a
-insertABL p (as,bs,ls) = case discrim p of
-  Alpha x y -> ((x,y):as , bs , ls)
-  Beta x y -> (as , (x,y):bs , ls)
-  Lit x -> (as , bs , x:ls)
-
-process2 :: ABL a -> [[Prop a]]
-process2 ([] , [] , []) = [[]]
-process2 (((x,y):as) , bs , ls) =
-  process2 $ insertABL x $ insertABL y (as,bs,ls)
-process2 (as , (x,y):bs , ls) =
-  process2 (insertABL x (as,bs,ls)) ++ process2 (insertABL y (as,bs,ls))
-process2 (as , bs , x:ls) = map (x:) $ process2 (as,bs,ls)
-
-tabulate2 :: Prop a -> [[Prop a]]
-tabulate2 p = process2 $ insertABL (NotP p) ([],[],[])
+-- tabulate :: Prop a -> [[Prop a]]
+-- tabulate p = process [NotP p]
 
 ----------------------------------------------------------------------
 
-toEither :: [Prop a] -> [Either a a]
+toEither :: [SProp a] -> [Either a a]
 toEither [] = []
-toEither (LetterP a : ps) = Left a : toEither ps
-toEither (NotP (LetterP a) : ps) = Right a : toEither ps
+toEither ((True , LetterP a) : ps) = Left a : toEither ps
+toEither ((False , LetterP a) : ps) = Right a : toEither ps
 toEither (_ : ps) = toEither ps
 
 both :: Eq a => [Either a a] -> Maybe [Either a a]
@@ -71,28 +57,28 @@ both = flip foldl (Just []) $
       Left p -> if any (== Right p) ps then Nothing else Just (Left p:ps)
       Right p -> if any (== Left p) ps then Nothing else Just (Right p:ps)
 
-contra :: Eq a => [Prop a] -> Bool
+contra :: Eq a => [SProp a] -> Bool
 contra = maybe True (const False) . both . toEither
 
-contras :: Eq a => [[Prop a]] -> Bool
+contras :: Eq a => [[SProp a]] -> Bool
 contras = all contra
 
 ----------------------------------------------------------------------
 
-tableau :: Eq a => Prop a -> Bool
-tableau = contras . tabulate
+-- tableau :: Eq a => Prop a -> Bool
+-- tableau = contras . tabulate
 
-tableau2 :: Eq a => Prop a -> Bool
-tableau2 = contras . tabulate2
+-- tableau2 :: Eq a => Prop a -> Bool
+-- tableau2 = contras . tabulate2
 
-----------------------------------------------------------------------
+-- ----------------------------------------------------------------------
 
-eg0 =  0 /\ 1
+-- eg0 =  0 /\ 1
 
-eg1 =  0 \/ (1 /\ 2)
+-- eg1 =  0 \/ (1 /\ 2)
 
-eg2 = 0 \/ (1 /\ 2) ~> ((0 \/ 1) /\ (0 \/ 2))
+-- eg2 = 0 \/ (1 /\ 2) ~> ((0 \/ 1) /\ (0 \/ 2))
 
-eg3 = (0 ~> (1 ~> 2)) ~> ((0 ~> 1) ~> (0 ~> 2))
+-- eg3 = (0 ~> (1 ~> 2)) ~> ((0 ~> 1) ~> (0 ~> 2))
 
-----------------------------------------------------------------------
+-- ----------------------------------------------------------------------
